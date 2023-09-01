b3f696e63df763b7f6951db9fccd2d6e108e2b50
juangruoficial <cruz9606@gmail.com>
2023-08-31 19:25:18 -0500

    Adding some styles for PokemonDetail

--- src/componets/pokemonDetail/Movements.jsx
+++ src/componets/pokemonDetail/Movements.jsx
@@ -1,20 +1,47 @@
+import { useEffect, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
+
 const Movements = ({ pokemonData }) => {
+  const [showMoves, setShowMoves] = useState(false);
+  const controls = useAnimation();
+
+  useEffect(() => {
+    if (pokemonData?.moves.length > 0) {
+      setShowMoves(true);
+    }
+  }, [pokemonData]);
+
+  useEffect(() => {
+    if (showMoves) {
+      controls.start((i) => ({
+        opacity: 1,
+        y: 0,
+        transition: { delay: i * 0.08 },
+      }));
+    }
+  }, [controls, showMoves]);
+
   return (
-    <section className="flex flex-col gap-5 mt-6 max-w-3xl  bg-white rounded-lg shadow-md p-6 ">
+    <section className="flex flex-col gap-5 mt-6 max-w-3xl bg-white rounded-lg shadow-md p-6">
       <h4 className="text-gray-600 font-semibold capitalize text-md md:text-lg lg:text-xl xl:text-2xl text-center border-b border-gray-300 pb-2 w-11/12 mx-auto md:w-full 2xl:text-4xl 3xl:text-5xl 4xl:text-6xl">
         Movements
       </h4>
       <div className="flex flex-wrap gap-2 mt-2 justify-center items-center">
-        {pokemonData?.moves.map((move) => (
-          <span
-            key={move}
-            className="bg-gray-200 rounded-full px-3 py-1 text-sm"
-          >
-            {move}
-          </span>
-        ))}
+        {showMoves &&
+          pokemonData.moves.map((move, index) => (
+            <motion.span
+              key={move}
+              custom={index}
+              initial={{ opacity: 0, y: -10 }}
+              animate={controls}
+              className="bg-gray-200 rounded-full px-3 py-1 text-sm"
+            >
+              {move}
+            </motion.span>
+          ))}
       </div>
     </section>
   );
 };
+
 export default Movements;
--- src/componets/pokemonDetail/PokemonInfo.jsx
+++ src/componets/pokemonDetail/PokemonInfo.jsx
@@ -3,34 +3,34 @@ import StatBarList from "./StatBarList";
 
 const PokemonInfo = ({ pokemonData }) => {
   return (
-    <article className="flex flex-col gap-6 w-full max-w-3xl bg-white rounded-lg shadow-md p-6">
+    <article className="flex flex-col gap-6 w-full max-w-3xl bg-white rounded-lg shadow-md p-6 mx-auto">
       <header
-        className={`relative h-24 md:h-32 lg:h-40 mt-5 mb-12 text-center ${
+        className={`relative h-32 md:h-40 mt-5 text-center ${
           bgStylePokemonType[pokemonData?.types[0]]
         } rounded-t-lg p-4`}
       >
-        <div className="mx-auto w-32 md:w-40 lg:w-48 absolute left-1/2 transform -translate-x-1/2">
+        <div className="mx-auto w-40 md:w-48 absolute left-1/2 transform -translate-x-1/2 -top-16">
           <img src={pokemonData?.image} alt="Pokemon" className="w-full" />
         </div>
       </header>
       <section>
         <article className="mb-4">
-          <span className="block text-center text-gray-600 border border-gray-300 rounded-md px-4 py-1 mb-2 m font-semibold text-xl md:text-2xl lg:text-3xl">
+          <span className="block text-center text-gray-600 border border-gray-300 rounded-md px-4 py-1 mb-2 font-semibold text-2xl md:text-3xl lg:text-4xl">
             #{pokemonData?.id}
           </span>
-          <h3 className="text-center text-2xl md:text-3xl font-semibold capitalize mt-2 mb-4 text-gray-800 border-b border-gray-300 pb-2 w-11/12 mx-auto md:w-full 2xl:text-4xl 3xl:text-5xl 4xl:text-6xl">
+          <h3 className="text-center text-3xl md:text-4xl font-semibold capitalize mt-2 mb-4 text-gray-800 border-b border-gray-300 pb-2 w-11/12 mx-auto">
             {pokemonData?.name}
           </h3>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
-            <div className="mb-2 md:mb-0 md:mr-4 flex flex-col gap-2 items-center w-full">
-              <p className="text-gray-600 md:text-lg lg:text-xl">Weight</p>
+          <div className="grid grid-cols-2 md:grid-cols-2 gap-4 mt-4">
+            <div className="mb-2 md:mb-0 flex flex-col gap-2 items-center w-full">
+              <p className="text-gray-600 text-lg md:text-xl">Weight</p>
               <p className="text-gray-800 font-semibold">
                 {pokemonData?.weight} kg
               </p>
             </div>
-            <div className="mb-2 md:mb-0 md:mr-4 flex flex-col gap-2 items-center w-full">
-              <p className="text-gray-600 md:text-lg lg:text-xl">Height</p>
+            <div className="mb-2 md:mb-0 flex flex-col gap-2 items-center w-full">
+              <p className="text-gray-600 text-lg md:text-xl">Height</p>
               <p className="text-gray-800 font-semibold">
                 {pokemonData?.height} cm
               </p>
@@ -38,16 +38,16 @@ const PokemonInfo = ({ pokemonData }) => {
           </div>
         </article>
 
-        <article className="flex flex-col mb-4 sm:flex-row gap-4 justify-between items-center w-full px-4 py-4 bg-gray-100 rounded-md sm:px-8 sm:py-8 lg:px-12 lg:py-12 xl:px-16 xl:py-16">
-          <div className="flex flex-col gap-5 justify-between items-center mb-2 px-4 py-4 bg-gray-100 rounded-md">
-            <h4 className="text-gray-600 font-semibold capitalize text-md md:text-lg lg:text-xl xl:text-2xl">
+        <article className="grid mb-4 md:grid-cols-2">
+          <div className="flex flex-col gap-2 justify-between items-center mb-4 px-4 py-4 bg-gray-100 rounded-md">
+            <h4 className="text-gray-600 font-semibold capitalize text-lg md:text-xl">
               Types
             </h4>
-            <div className="flex gap-2 items-center">
+            <div className="flex gap-2 items-center justify-center">
               {pokemonData?.types.map((type) => (
                 <span
                   key={type}
-                  className="text-gray-600 capitalize bg-gray-200 rounded-full px-3 py-1 text-sm"
+                  className={`text-sm px-3 py-2 rounded-full font-semibold ${bgStylePokemonType[type]} text-white`}
                 >
                   {type}
                 </span>
@@ -55,20 +55,20 @@ const PokemonInfo = ({ pokemonData }) => {
             </div>
           </div>
 
-          <div className="flex flex-col gap-5 justify-between items-center mb-2 px-4 py-4 bg-gray-100 rounded-md">
-            <p className="text-gray-600 font-semibold capitalize text-md md:text-lg lg:text-xl xl:text-2xl">
+          <div className="flex flex-col gap-2 justify-between items-center mb-4 px-4 py-4 bg-gray-100 rounded-md">
+            <p className="text-gray-600 font-semibold capitalize text-lg md:text-xl">
               Abilities
             </p>
-            <p className="text-gray-800 font-semibold">
+            <div className="flex flex-wrap gap-2">
               {pokemonData?.abilities.map((ability) => (
                 <span
                   key={ability}
-                  className="capitalize bg-gray-200 px-3 py-1 text-sm rounded-full mr-2 mb-2 inline-block text-gray-600 "
+                  className="capitalize bg-gray-200 px-3 py-1 text-sm rounded-full text-gray-600"
                 >
                   {ability}
                 </span>
               ))}
-            </p>
+            </div>
           </div>
         </article>
 
@@ -77,4 +77,5 @@ const PokemonInfo = ({ pokemonData }) => {
     </article>
   );
 };
+
 export default PokemonInfo;
